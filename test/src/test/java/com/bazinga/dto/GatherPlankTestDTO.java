f2ecfed3bb6774b4e767c9493fc68c5e92f09e12
Administrator <511632242@qq.com>
2022-07-24 16:25:57 +0800

    dafda

---
+++ test/src/test/java/com/bazinga/dto/GatherPlankTestDTO.java
@@ -0,0 +1,18 @@
+package com.bazinga.dto;
+
+
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+@Data
+public class GatherPlankTestDTO {
+    private String stockCode;
+    private String stockName;
+    private String tradeDate;
+    private BigDecimal tenDayRate;
+    private int tenDayPlanks;
+    private int continuePlanks;
+    private BigDecimal profit;
+
+}
